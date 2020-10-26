ee109826c9bb500d7ae889d84261c33e21a3997f
Jahanzaib Ali | Sr. Java Developer or Java Full-Stack Developer <jahanzaib_ali1@outlook.com>
2020-10-26 12:23:37 +0100

    updated

--- src/test/java/appium/test/apk/util/Constant.java
+++ src/test/java/appium/test/apk/util/Constant.java
@@ -1,5 +1,5 @@
 package appium.test.apk.util;
 
 final public class Constant {
-	public static final String APPIUM_SERVER_URL = "http://172.26.111.151:4723/wd/hub";
+	public static final String APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub";
 }
